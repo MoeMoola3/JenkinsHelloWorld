5076df1ab086ac05e5f9b9ced943e40353457128
Mohammed Moola <73942516+MoeMoola3@users.noreply.github.com>
2024-03-12 13:21:48 +0200

    Update JenkinsHelloWorld.java

--- JenkinsHelloWorld.java
+++ JenkinsHelloWorld.java
@@ -1,5 +1,5 @@
 public class JenkinsHelloWorld {
     public static void main(String[] args) {
-        System.out.println("Jenkins Hello World!");
+        System.out.println("Jenkins Edit the File!");
     }
 }
